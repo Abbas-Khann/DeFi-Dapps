0bd7335222dcdef2494a86f0765e10183ed70942
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-08-29 20:02:14 +0430

    Smart Contract finished

--- Vault_Dapp/contracts/Vault.sol
+++ Vault_Dapp/contracts/Vault.sol
@@ -10,3 +10,70 @@ contract GoldToken is ERC20, ERC20Burnable {
     }
 }
 
+/*
+-> We should have a mint function to deposit 
+-> We should also have a deposit function to deposit tokens that the ERC20 contract approves us to use
+-> We should have a withdraw function which requires a burn function
+*/
+
+contract Vault {
+    IERC20 public immutable token;
+    uint256 public totalSupply;
+
+    mapping (address => uint256) public balanceOf;
+    
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    // When we mint we increment(add)
+    function mint(address _to, uint256 _shares) private {
+        totalSupply += _shares;
+        balanceOf[_to] += _shares;
+    }
+
+    // When we burn we decrement or just remove
+    function burn(address _from, uint256 _shares) private {
+        totalSupply -= _shares;
+        balanceOf[_from] -= _shares;
+    }
+
+    function deposit(uint256 _amount) external {
+         /*
+        a = amount
+        B = balance of token before deposit
+        T = total supply
+        s = shares to mint
+
+        (T + s) / T = (a + B) / B 
+
+        s = aT / B
+        */
+        uint256 shares;
+        if (totalSupply == 0) {
+            shares = _amount;
+        }
+        else {
+            shares = (_amount * totalSupply) / token.balanceOf(address(this));
+        }
+        mint(msg.sender, shares);
+        token.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    function withdraw(uint256 _shares) public {
+        /*
+        a = amount
+        B = balance of token before withdraw
+        T = total supply
+        s = shares to burn
+
+        (T - s) / T = (B - a) / B 
+
+        a = sB / T
+        */
+        uint256 _amount = (_shares * token.balanceOf(address(this))) / totalSupply;
+        burn(msg.sender, _shares);
+        token.transfer(msg.sender, _amount);
+    }
+
+}
