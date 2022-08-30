25f82c1791a5378a19c4e471225fcc6bdb26fe09
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-08-31 00:27:11 +0430

    Finished DarkMode and lightMode

--- Vault_Dapp/frontend/components/Navbar.tsx
+++ Vault_Dapp/frontend/components/Navbar.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head';
 import { BsLightbulbFill } from 'react-icons/bs';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-const Navbar = () => {
+const Navbar = (props: any) => {
+   const {toggleDarkMode, darkMode} = props 
+
   return (
-    <nav className='flex items-center justify-center px-6 py-4 bg-gradient-to-r from-[#b7c3d3] to-[whitesmoke] shadow-2xl'>
-        <Head>
+    <div className={`${darkMode && 'dark'} bg-gradient-to-r from-[#b7c3d3] to-[whitesmoke]`}>
+      <Head>
              <style>
                @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@600&display=swap');
              </style>
         </Head>
+        <nav className="flex items-center justify-center px-6 py-4 dark:bg-gradient-to-r from-[#212b3c] to-[#112B3C] dark:text-white shadow-2xl dark:shadow-lg dark:shadow-blue-500/50">
         <h1 className='text-xl sm:text-3xl'>Vault Dapp</h1>
         <div className='flex flex-auto justify-end items-center px-4'>
-        <BsLightbulbFill className='text-3xl cursor-pointer mr-4'/>
+        {darkMode ? 
+        <BsLightbulbFill
+        onClick={toggleDarkMode} 
+        className='text-3xl cursor-pointer mr-4 text-yellow-400'
+        />
+        :
+        <BsLightbulbFill
+        onClick={toggleDarkMode}
+        className='text-3xl cursor-pointer mr-4'/>
+    }
         <ConnectButton />
         </div>
     </nav>
+    </div>
   )
 }
 
--- Vault_Dapp/frontend/pages/index.tsx
+++ Vault_Dapp/frontend/pages/index.tsx
@@ -4,10 +4,16 @@ import Navbar from '../components/Navbar';
 
 const Home: NextPage = () => {
 
+  const [darkMode, setDarkMode] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [isApproved, setIsApproved] = useState<boolean>(false);
   const [isDeposited, setIsDeposited] = useState<boolean>(false);
   const [isWithdrawn, setIsWithdrawn] = useState<boolean>(false);
+
+  function toggleDarkMode(): void {
+    setDarkMode(!darkMode);
+  }
+
 
   const renderButton = (): JSX.Element => {
     if(isWithdrawn) {
@@ -35,22 +41,24 @@ const Home: NextPage = () => {
     }
   }
   return (
-    <main className='bg-gradient-to-r from-[#6FB2D2] to-[#D8D2CB] min-h-screen'> 
-      <Navbar />
+    <main className={`${darkMode && "dark"} bg-gradient-to-r from-[#6FB2D2] to-[#D8D2CB] min-h-screen`}> 
+    <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode}/>
+    <section className='dark:bg-gradient-to-r from-[#121212] to-[#002B5B] dark:text-white min-h-screen'>
       <div className='flex justify-center'>
         <h3 className='text-2xl pt-12 inline-block border-b-4 border-[#7084a0] sm:text-4xl '>Double Your Tokens with the Vault</h3>
       </div>
       <div className='sm:flex sm:items-center sm:justify-center py-16 px-20'>
       <div className=''>
-        <p className='text-xl sm:text-2xl py-4'>You own 0/1000 Gold Tokens</p>
+        <p className='text-2xl sm:text-3xl py-4'>You own 0/1000 Gold Tokens</p>
           {renderButton()}
       </div>
       <div className='sm:ml-28'>
-      {/* <img src="https://img.icons8.com/external-flat-wichaiwi/184/000000/external-token-gamefi-flat-wichaiwi.png"/> */}
-      {/* <img src="https://img.icons8.com/external-ddara-lineal-ddara/184/000000/external-tokens-gaming-gambling-ddara-lineal-ddara.png"/> */}
+      {darkMode ? <img src="https://img.icons8.com/external-flat-wichaiwi/184/000000/external-token-gamefi-flat-wichaiwi.png"/> : 
       <img src="https://img.icons8.com/external-glyph-wichaiwi/184/000000/external-token-gamefi-glyph-wichaiwi.png"/>
+      }
       </div>
       </div>
+    </section>
     </main>
   )
 }
