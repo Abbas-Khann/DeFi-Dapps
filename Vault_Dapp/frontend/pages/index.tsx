fc39dac224bd859fb9cedd458ca45138d9548e54
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-01 19:02:25 +0430

    Functions fetched from the vault contract

--- Vault_Dapp/frontend/pages/index.tsx
+++ Vault_Dapp/frontend/pages/index.tsx
@@ -18,6 +18,8 @@ const Home: NextPage = () => {
   const [tokensToApprove, setTokensToApprove] = useState<number>(0);
   const [totalSupply, setTotalSupply] = useState<number>(0);
   const [approvedTokensAmount, setApprovedTokensAmount] = useState<number>(0);
+  const [depositedTokensAmount, setDepositedTokensAmount] = useState<number>(0);
+  const [inputValue, setInputValue] = useState<number>(0);
 
   function toggleDarkMode(): void {
     setDarkMode(!darkMode);
@@ -41,8 +43,8 @@ const Home: NextPage = () => {
     try {
       const total: number = await tokenContract.totalSupply();
       setTotalSupply(total);
-    } catch (err) {
-      console.log('Error fetching the total Supply')
+    } catch (err: any) {
+      alert(err.reason)
       console.error(err)
     }
   }
@@ -52,10 +54,12 @@ const Home: NextPage = () => {
       const tx: any = await tokenContract.approve(spender, amount);
       setLoading(true);
       await tx.wait();
-      setIsApproved(true);
       setLoading(false);
+      setIsApproved(true);
+      checkApproval(amount)
     }
-    catch (err) {
+    catch (err: any) {
+      alert(err.reason)
       console.error(err)
     }
   }
@@ -64,29 +68,79 @@ const Home: NextPage = () => {
     try {
       const approved: number = await tokenContract.checkApproval(_amount);
       setApprovedTokensAmount(+approved);
+      if(approvedTokensAmount > 0) {
+        setIsApproved(true)
+      }
     } catch (err) {
       console.error(err)
     }
   }
 
-  console.table(approvedTokensAmount)
+  const checkIfApproved = () => {
+    try {
+      if(approvedTokensAmount > 0) {
+        setIsApproved(true);
+      }
+    } catch (err) {
+      console.error(err)
+      console.error("Executed")
+    }
+  }
+
+  const depositTokens = async (_amount: number): Promise<void> => {
+    try{
+      const tx = await vaultContract.deposit(_amount);
+      setLoading(true);
+      await tx.wait();
+      setLoading(false)
+      setIsDeposited(true);
+    }
+    catch(err: any) {
+      alert(err.reason)
+      console.error(err)
+    }
+  }
+
+  const totalSupplyAtVault = async (): Promise<void> => {
+    try {
+      const total: number = await vaultContract.totalSupply();
+      console.log(total.toString())
+    }
+     catch (err: any) {
+      alert(err.reason);
+      console.error(err)
+    }
+  }
+
 
   function fetchInputVal(event: any): void {
     setTokensToApprove(+event.target.value);
-    console.log(tokensToApprove)
-    console.log(typeof(tokensToApprove))
+  }
+
+  function fetchInputValue(event: any): void {
+    setInputValue(+event.target.value);
   }
 
   useEffect(() => {
     getTotalSupply();
-    checkApproval(tokensToApprove);
+    // checkApproval(2)
+    checkIfApproved();
   }, [])
+
+  useEffect(() => {
+    totalSupplyAtVault();
+  }, [totalSupplyAtVault, isDeposited])
+
+  setTimeout(() => {
+    checkIfApproved()
+  })
 
 
   const renderButton = (): JSX.Element => {
     if(isWithdrawn) {
       return (
       <div>
+      <p className='text-2xl sm:text-3xl py-4'>You own {totalSupply.toString()} Gold Tokens now</p>
       <p className='transition duration-300 ease-out hover:ease-in text-3xl rounded py-2 dark:text-white mb-3'>
       Astaghfirullah!!! You haram khor you wanted to commit Riba????<br /> Your tokens have been halved now for sinning.
       </p>
@@ -99,22 +153,45 @@ const Home: NextPage = () => {
       </button>
     }
     else if(isApproved) {
-      return <button className='px-4 py-2 my-1 border-2 transition duration-300 motion-safe:animate-bounce ease-out hover:ease-in hover:bg-gradient-to-r from-[#5463FF] to-[#89CFFD] text-3xl rounded hover:text-white mb-3'>Deposit</button>
+      return (
+        <div>
+          <p className='text-2xl sm:text-3xl py-4'>{tokensToApprove.toString()}/{totalSupply.toString()} Tokens have been approved</p>
+          <div className='flex flex-col w-40'>
+          <button className='px-4 py-2 my-1 border-2 transition duration-300 motion-safe:animate-bounce ease-out hover:ease-in hover:bg-gradient-to-r from-[#5463FF] to-[#89CFFD] text-3xl rounded hover:text-white mb-3'
+          onClick={() => depositTokens(inputValue)}
+          >Deposit</button>
+          <input
+          onChange={fetchInputValue} 
+          className=' text-black text-2xl text-center border-2 dark:text-white font-bold dark:bg-gradient-to-r dark:bg-clip-text dark:text-transparent 
+          dark:from-red-400 dark:via-purple-500 dark:to-white
+          dark:animate-text sm:w-40'
+          placeholder='Enter Amount'
+          type="number"
+          />
+          </div>
+        </div>
+      )
     }
     else if(isDeposited) {
-      return <button className='px-4 py-2 my-1 border-2 transition duration-300 motion-safe:animate-bounce ease-out hover:ease-in hover:bg-gradient-to-r from-[#5463FF] to-[#89CFFD] text-3xl rounded hover:text-white mb-3'>Withdraw</button>
+      return (
+        <div>
+          <p className='text-2xl sm:text-3xl py-4'>{depositedTokensAmount.toString()}/{tokensToApprove.toString()} Tokens have been deposited</p>
+          <button className='px-4 py-2 my-1 border-2 transition duration-300 motion-safe:animate-bounce ease-out hover:ease-in hover:bg-gradient-to-r from-[#5463FF] to-[#89CFFD] text-3xl rounded hover:text-white mb-3'>Withdraw</button>
+        </div>
+        )
     }
     else {
       return (
-      <div className='flex flex-col w-40 sm:w-40'>
-      <button className='px-4 py-2 my-1 border-2 transition duration-300 motion-safe:animate-bounce ease-out hover:ease-in hover:bg-gradient-to-r from-[#5463FF] to-[#89CFFD] text-3xl rounded hover:text-white mb-3'
+      <div className='flex flex-col'>
+      <p className='text-2xl sm:text-3xl py-4'>You own {totalSupply.toString()}/{totalSupply.toString()} Gold Tokens</p>
+      <button className='px-4 py-2 my-1 border-2 transition duration-300 motion-safe:animate-bounce ease-out hover:ease-in hover:bg-gradient-to-r from-[#5463FF] to-[#89CFFD] text-3xl rounded hover:text-white mb-3 sm:w-40'
       onClick={() => approveTokensToVault(VAULT_CONTRACT_ADDRESS, tokensToApprove)}
       >Approve</button>
       <input
       onChange={fetchInputVal} 
       className=' text-black text-2xl text-center border-2 dark:text-white font-bold dark:bg-gradient-to-r dark:bg-clip-text dark:text-transparent 
       dark:from-red-400 dark:via-purple-500 dark:to-white
-      dark:animate-text'
+      dark:animate-text sm:w-40'
       placeholder='Enter Amount'
       type="number"
       />
@@ -136,10 +213,9 @@ const Home: NextPage = () => {
       </div>
       <div className='sm:flex sm:items-center sm:justify-center py-16 px-20'>
       <div className=''>
-        <p className='text-2xl sm:text-3xl py-4'>You own 0/{totalSupply.toString()} Gold Tokens</p>
           {renderButton()}
       </div>
-      <div className='sm:ml-28'>
+      <div className='mt-5 sm:ml-28'>
       {darkMode ? <img src="https://img.icons8.com/external-flat-wichaiwi/184/000000/external-token-gamefi-flat-wichaiwi.png"/> : 
       <img src="https://img.icons8.com/external-glyph-wichaiwi/184/000000/external-token-gamefi-glyph-wichaiwi.png"/>
       }
