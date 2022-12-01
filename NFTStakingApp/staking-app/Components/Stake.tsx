b1960f266d69d53801933012dbadaefad5f749b8
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-12-01 20:16:36 +0800

    Stake component completed

--- NFTStakingApp/staking-app/Components/Stake.tsx
+++ NFTStakingApp/staking-app/Components/Stake.tsx
@@ -1,14 +1,30 @@
 import React,{ useState } from "react";
 import styles from "../styles/Home.module.css";
+import { useContract } from "@thirdweb-dev/react";
 
 const Stake = (): JSX.Element => {
 
+    const STAKING_CONTRACT_ADDRESS: string = "0x678deE906a62f540a33156fe54dd8b321a37fB1B";
+    const NFT_CONTRACT_ADDRESS: string = "0x24c5258769d49951fb2Fffb0c1948B9A53db12D6";
     const [inputValue, setInputValue] = useState<number>();
+
+    const { contract } = useContract(STAKING_CONTRACT_ADDRESS, "custom");
+    const { contract: nftDropContract } = useContract(NFT_CONTRACT_ADDRESS, "nft-drop");
 
     function handleChange(e: any): void {
         setInputValue(+e.target.value);
         console.log(inputValue)
     }
+
+    const stakeNFT = async (): Promise<void> => {
+        const stake = await contract?.call("stakeNft", inputValue)
+    }
+
+    const approveNft = async (): Promise<void> => {
+        const approve = await nftDropContract?.call("approve", STAKING_CONTRACT_ADDRESS, inputValue);
+        await stakeNFT();
+    }
+
 
     return(
         <div className={styles.tokenGrid}>
@@ -23,6 +39,7 @@ const Stake = (): JSX.Element => {
                 <br />
                 <button
                 className={`${styles.mainButton} ${styles.spacerTop}`}
+                onClick={approveNft}
                 >
                     Stake
                 </button>
