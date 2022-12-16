dcaff29c1aee46a288c0ef672a788408a2870851
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-12-16 12:18:58 +0800

    Added comments to components

--- NFTStakingApp/staking-app/Components/Rewards.tsx
+++ NFTStakingApp/staking-app/Components/Rewards.tsx
@@ -5,8 +5,10 @@ import {useContract} from "@thirdweb-dev/react";
 const Rewards = (): JSX.Element => {
 
     const STAKING_CONTRACT_ADDRESS : string = "0x678deE906a62f540a33156fe54dd8b321a37fB1B";
+    // pasting the staking contract address variable in the useContract hook
     const { contract } = useContract(STAKING_CONTRACT_ADDRESS);
 
+    // claimRewards will be an async function and it will call the claimRewards function from the staking contract
     const claimRewards = async (): Promise<void> => {
         const claim = await contract?.call("claimRewards");
     }
@@ -15,6 +17,7 @@ const Rewards = (): JSX.Element => {
         <div className={styles.rewardsDiv}>
                 <button
                 className={`${styles.mainButton} ${styles.spacerTop}`}
+                // calling the claimRewards function here
                 onClick={() => claimRewards()}
                 >
                 Claim Rewards
--- NFTStakingApp/staking-app/Components/Stake.tsx
+++ NFTStakingApp/staking-app/Components/Stake.tsx
@@ -4,24 +4,30 @@ import { useContract } from "@thirdweb-dev/react";
 
 const Stake = (): JSX.Element => {
 
-    const STAKING_CONTRACT_ADDRESS: string = "0x678deE906a62f540a33156fe54dd8b321a37fB1B";
-    const NFT_CONTRACT_ADDRESS: string = "0x24c5258769d49951fb2Fffb0c1948B9A53db12D6";
+    const STAKING_CONTRACT_ADDRESS = "0x678deE906a62f540a33156fe54dd8b321a37fB1B";
+    const NFT_CONTRACT_ADDRESS = "<paste-NFT-contract-address-here>";
+    // taking the input value in this state
     const [inputValue, setInputValue] = useState<number>();
-
+    // pasting the contract address and the type
     const { contract } = useContract(STAKING_CONTRACT_ADDRESS, "custom");
+    // adding the type of the contract and the address here again
     const { contract: nftDropContract } = useContract(NFT_CONTRACT_ADDRESS, "nft-drop");
 
+    // picking up the value using this function and converting it into a number with the +
     function handleChange(e: any): void {
         setInputValue(+e.target.value);
-        console.log(inputValue)
     }
 
+    // stakeNFT will be used to Stake the NFT
     const stakeNFT = async (): Promise<void> => {
+        // calling the stakeNft function from the contract and passing in the inputValue state
         const stake = await contract?.call("stakeNft", inputValue)
     }
 
+    // The token needs to be approved before it can be used for staking so we will approve the particular NFT before performing the staking
     const approveNft = async (): Promise<void> => {
         if(inputValue) {
+            // Here we are approving the NFT to the staking contract with the tokenID
             const approve = await nftDropContract?.call("approve", STAKING_CONTRACT_ADDRESS, inputValue);
             await stakeNFT();
         }
@@ -30,8 +36,10 @@ const Stake = (): JSX.Element => {
         }
     }
 
+    // NFT withdrawal here
     const withdrawNft = async (): Promise<void> => {
         if(inputValue) {
+            // similar process as the stakeNft function but this will withdraw the NFT 
             const withdraw = await contract?.call("withdrawNFT", inputValue);
         }
         else {
@@ -48,11 +56,13 @@ const Stake = (): JSX.Element => {
                 type="number"
                 className={styles.input}
                 placeholder="Token ID here"
+                // onChange we will call the handleChange function to pick up the values
                 onChange={handleChange}
                 />
                 <br />
                 <button
                 className={`${styles.mainButton} ${styles.spacerTop}`}
+                // onClick we will call the approveNft function
                 onClick={approveNft}
                 >
                     Stake
@@ -69,6 +79,7 @@ const Stake = (): JSX.Element => {
                 <br />
                 <button
                 className={`${styles.mainButton} ${styles.spacerTop}`}
+                // onClick we will call the withdrawal function
                 onClick={withdrawNft}
                 >
                     Withdraw
