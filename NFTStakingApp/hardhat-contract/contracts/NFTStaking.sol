75ffd5f1a29a2361c264a3f68eda7d2cdced0bdd
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-01-06 11:22:15 +0800

    Removed Unnecessary code

--- NFTStakingApp/hardhat-contract/contracts/NFTStaking.sol
+++ NFTStakingApp/hardhat-contract/contracts/NFTStaking.sol
@@ -7,7 +7,7 @@ contract MyToken is Staking721Base {
 
     // This contract allows users to stake their nft for 30 days
     // User can only withdraw their NFT after 30 days of time limit is reached
-    // User can earn staking rewards anytime
+    // User can earn and claim their staking rewards anytime
         
       mapping (uint256 => bool) public nftLocked;
       mapping (uint256 => uint256) public nftLockTime;
@@ -26,79 +26,31 @@ contract MyToken is Staking721Base {
     {}
 
     function _stake(uint256[] calldata _tokenIds) internal override {
-        uint256 len = _tokenIds.length;
-        require(len != 0, "Staking 0 tokens");
-
-        address _nftCollection = nftCollection;
-
-        if (stakers[msg.sender].amountStaked > 0) {
-            _updateUnclaimedRewardsForStaker(msg.sender);
-        } else {
-            stakersArray.push(msg.sender);
-            stakers[msg.sender].timeOfLastUpdate = block.timestamp;
-        }
-        for (uint256 i = 0; i < len; ++i) {
-            require(
-                IERC721(_nftCollection).ownerOf(_tokenIds[i]) == msg.sender &&
-                    (IERC721(_nftCollection).getApproved(_tokenIds[i]) == address(this) ||
-                        IERC721(_nftCollection).isApprovedForAll(msg.sender, address(this))),
-                "Not owned or approved"
-            );
-            IERC721(_nftCollection).transferFrom(msg.sender, address(this), _tokenIds[i]);
-            stakerAddress[_tokenIds[i]] = msg.sender;
-
-            if (!isIndexed[_tokenIds[i]]) {
-                isIndexed[_tokenIds[i]] = true;
-                indexedTokens.push(_tokenIds[i]);
-            }
+        super._stake(_tokenIds);
+        for (uint i = 0; i < _tokenIds.length; i++) {
             nftLocked[_tokenIds[i]] = true;
             nftLockTime[_tokenIds[i]] = block.timestamp + 30 days;
         }
-        stakers[msg.sender].amountStaked += len;
-        emit TokensStaked(msg.sender, _tokenIds);
     }
 
-    function stakeNft(uint256[] calldata _tokenIds) external {
+     function stakeNft(uint256[] calldata _tokenIds) public {
         _stake(_tokenIds);
     }
-    
 
     function _withdraw(uint256[] calldata _tokenIds) internal override {
-        uint256 _amountStaked = stakers[msg.sender].amountStaked;
-        uint256 len = _tokenIds.length;
-        require(len != 0, "Withdrawing 0 tokens");
-        require(_amountStaked >= len, "Withdrawing more than staked");
-
-        address _nftCollection = nftCollection;
-
-        _updateUnclaimedRewardsForStaker(msg.sender);
-
-        if (_amountStaked == len) {
-            for (uint256 i = 0; i < stakersArray.length; ++i) {
-                if (stakersArray[i] == msg.sender) {
-                    stakersArray[i] = stakersArray[stakersArray.length - 1];
-                    stakersArray.pop();
-                }
-            }
-        }
-        stakers[msg.sender].amountStaked -= len;
-
-        for (uint256 i = 0; i < len; ++i) {
-            require(stakerAddress[_tokenIds[i]] == msg.sender, "Not staker");
-            require(
+        super._withdraw(_tokenIds);
+        for(uint i = 0; i < _tokenIds.length; i++) {
+         require(
             block.timestamp > nftLockTime[_tokenIds[i]],
             "You can't withdraw unless your 30 days are completed!"
             );
             nftLocked[_tokenIds[i]] = false;
-            stakerAddress[_tokenIds[i]] = address(0);
-            IERC721(_nftCollection).transferFrom(address(this), msg.sender, _tokenIds[i]);
         }
-
-        emit TokensWithdrawn(msg.sender, _tokenIds);
     }
 
-    function withdrawNFT(uint256[] calldata _tokenIds) external {
+    function withdrawNFT(uint256[] calldata _tokenIds) public {
         _withdraw(_tokenIds);
     }
 
 }
+
